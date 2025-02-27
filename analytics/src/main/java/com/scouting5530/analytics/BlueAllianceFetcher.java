ad7dee040b0f951c16107f44530d3977c896a106
Adhrit Bharath <abharath@greenhillsschool.org>
2025-02-27 16:27:27 -0500

    first commit

---
+++ analytics/src/main/java/com/scouting5530/analytics/BlueAllianceFetcher.java
@@ -0,0 +1,102 @@
+package com.scouting5530.analytics;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+
+
+public class BlueAllianceFetcher {
+    public final String api_url = "https://www.thebluealliance.com/api/v3";
+    public final String api_key = "ME9K7byqRTWn6vvWDvzCncK0Io80Q4UvUt2Kbddp9dhUyW7QN5ex0POQmjvVnnfp";
+    public final String event_id = "2024miann";
+    private File ferndaleTeams;
+
+
+    /* /team/{team_key}/event/{event_key}/matches */
+    public JSONArray getMatches(int team) throws URISyntaxException, IOException, InterruptedException{
+        String url = api_url + "/team/" + getTeamId(team) + "/event/" + event_id + "/matches";
+        URI uri = new URI(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(uri)
+        .header("X-TBA-Auth-Key", api_key)
+        .GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONArray out = new JSONArray();
+        if (response.statusCode()==200) {
+            out = new JSONArray(response.body());
+        }
+       
+
+        return out;
+
+
+    }
+    public List<Match> getOpps(JSONArray matches, int teamNum) {
+        List<Match> out = new ArrayList<>();
+        for (int i = 0; i < matches.length(); i++) {
+            JSONObject obj = (JSONObject) matches.get(i);
+            int matchNum = obj.getInt("match_number");
+            JSONObject red = obj.getJSONObject("alliances").getJSONObject("red");
+            JSONObject blue = obj.getJSONObject("alliances").getJSONObject("blue");
+            JSONArray keys = red.getJSONArray("team_keys");
+            boolean useRed = true;
+            for (Object o : keys) {
+                if (((String)o).equals(getTeamId(teamNum))) {
+                    useRed = false;
+                }
+            }
+            keys = useRed ? keys : blue.getJSONArray("team_keys");
+            for (Object o : keys) {
+                String id = (String)o;
+                int num = Integer.parseInt(id.substring(3));
+                Match m = new Match(num, matchNum);
+                out.add(m);
+            }
+
+
+        }
+   
+        return out;
+    }
+    public List<Match> getOpps(int teamNum)throws URISyntaxException, IOException, InterruptedException {
+        return getOpps(getMatches(teamNum), teamNum);
+    }
+    public class Match {
+        int team;
+        int qual;
+        public Match() {
+            team = 0;
+            qual = 0;
+        }
+        public Match(int team, int qual) {
+            this.team = team;
+            this.qual = qual;
+        }
+        @Override
+        public String toString() {
+            return "(" + Integer.toString(team) + "," + Integer.toString(qual) + ")";
+        }
+    }
+  
+    /* /event/{event_key}/teams */
+    public String getTeamId(int teamId){
+        return "frc" + Integer.toString(teamId);
+
+ 
+    }
+    
+}
+
+
+ 
---
+++ analytics/src/main/java/com/scouting5530/analytics/SheetsQuickstart.java
@@ -0,0 +1,97 @@
+package com.scouting5530.analytics;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+public class SheetsQuickstart {
+  private static final String APPLICATION_NAME = "Green Goggles";
+  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+  private static final String TOKENS_DIRECTORY_PATH = "tokens";
+  private static final String SPREADSHEET_ID  = "1fmYuVSqP3JfEZXwsBNbUWnL-HnkDAWfgd_0JHj8GOzQ";
+  Sheets service;
+  /**
+   * Global instance of the scopes required by this quickstart.
+   * If modifying these scopes, delete your previously saved tokens/ folder.
+   */
+  private static final List<String> SCOPES =
+      Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
+  private static final String CREDENTIALS_FILE_PATH = "/client_secret_385220316235-dviq43cq8k960vjjotc3jjk0lv8sl87b.apps.googleusercontent.com.json";
+
+  /**
+   * Creates an authorized Credential object.
+   *
+   * @param HTTP_TRANSPORT The network HTTP Transport.
+   * @return An authorized Credential object.
+   * @throws IOException If the credentials.json file cannot be found.
+   */
+  private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+      throws IOException {
+    // Load client secrets.
+    InputStream in = SheetsQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+    if (in == null) {
+      throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+    }
+    GoogleClientSecrets clientSecrets =
+        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+    // Build flow and trigger user authorization request.
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+        .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+        .setAccessType("offline")
+        .build();
+    LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+    return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+  }
+
+
+  public void initialize() {
+    try {
+     // Build a new authorized API client service.
+     final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+     
+     Sheets service =
+         new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+             .setApplicationName(APPLICATION_NAME)
+             .build();
+     this.service = service;
+  }
+
+  catch (Exception ex) {
+    ex.printStackTrace();
+  }
+}
+
+  public ValueRange getValues(String range) throws IOException {
+  
+    return service.spreadsheets().values().get(SPREADSHEET_ID, range).execute();
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
